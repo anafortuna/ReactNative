6e312cba9e9d1a28805dfa80a759bd5271900679
anafortuna <alobofortuna@gmail.com>
2024-11-07 21:52:24 -0300

    Primeiro commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Image, Text, TouchableOpacity, View } from 'react-native';
+import { styles } from './styles';
+import CircleIcon from './CircleIcon.png';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  return <View style={styles.container}>
+    <Text style={styles.title}>
+      Skills
+    </Text>
+    <TouchableOpacity activeOpacity={0.6} style={styles.buttonSkill}>
+    <Image source={CircleIcon} style={styles.image}/>
+      <Text style={styles.textSkill}>
+        Habilidade 1
+      </Text>
+    </TouchableOpacity>
+    <TouchableOpacity style={styles.buttonSkill}>
+    <Image source={CircleIcon} style={styles.image}/>
+      <Text style={styles.textSkill}>
+        Habilidade 2
+      </Text>
+    </TouchableOpacity>
+    <TouchableOpacity style={styles.buttonSkill}>
+    <Image source={CircleIcon} style={styles.image}/>
+      <Text style={styles.textSkill}>
+        Habilidade 3
+      </Text>
+    </TouchableOpacity>
+  </View>
+}
